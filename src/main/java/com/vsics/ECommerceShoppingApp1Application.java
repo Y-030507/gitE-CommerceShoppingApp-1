b87b263c8f56d02e92bbdee6e6e245b3f720fdc7
YashsviSingh <yashsvisingh3@gmail.com>
2024-07-06 03:52:02 +0530

    First Commit

---
+++ src/main/java/com/vsics/ECommerceShoppingApp1Application.java
@@ -0,0 +1,13 @@
+package com.vsics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceShoppingApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceShoppingApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsics/ECommerceShoppingApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShoppingApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
